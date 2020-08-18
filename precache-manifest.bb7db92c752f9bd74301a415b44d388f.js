37754796a76c117aa015d6002b3fbe54c2c48f77
Sam McMurray <scmcmur@gmail.com>
2020-08-17 21:23:57 -0400

    Updates

---
+++ precache-manifest.bb7db92c752f9bd74301a415b44d388f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0f3d29844bda2301080d760c2c900a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "98988aff0c1d6e00cfb6",
+    "url": "/portfolio/static/css/main.3582c37e.chunk.css"
+  },
+  {
+    "revision": "f426bfa5bcf13fe825ef",
+    "url": "/portfolio/static/js/2.1ffb51c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1ffb51c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98988aff0c1d6e00cfb6",
+    "url": "/portfolio/static/js/main.ff09c1dd.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
